6dbf6c270a002441aa36fbd426fda3d5370d458f
bartolomeusantos072 <bartolomeusantos072@gmail.com>
2023-09-28 12:37:16 -0300

    my sql

--- dump/escola.sql
+++ dump/escola.sql
@@ -10,3 +10,26 @@ CREATE TABLE aluno (
     sexo_al CHAR(1),
     email_alu VARCHAR(255)
 );
+
+INSERT INTO aluno (nome_alu, cidade_alu, tel_alu, sexo_al, email_alu)
+VALUES
+    ('Albert Einstein', 'Berlim', '123-456-7890', 'M', 'einstein@example.com'),
+    ('Marie Curie', 'Varsóvia', '987-654-3210', 'F', 'curie@example.com'),
+    ('Isaac Newton', 'Woolsthorpe', '555-555-5555', 'M', 'newton@example.com'),
+    ('Charles Darwin', 'Shrewsbury', '111-222-3333', 'M', 'darwin@example.com'),
+    ('Galileo Galilei', 'Pisa', '999-888-7777', 'M', 'galilei@example.com'),
+    ('Ada Lovelace', 'Londres', '777-777-7777', 'F', 'lovelace@example.com'),
+    ('Nikola Tesla', 'Smiljan', '444-333-2222', 'M', 'tesla@example.com'),
+    ('Alan Turing', 'Maida Vale', '222-333-4444', 'M', 'turing@example.com'),
+    ('Stephen Hawking', 'Oxford', '666-555-4444', 'M', 'hawking@example.com'),
+    ('Jane Goodall', 'Londres', '777-666-5555', 'F', 'goodall@example.com'),
+    ('Roberto','Rio Pomba','123-4536-7890','M','roberto@oi.com.br');
+
+SELECT * FROM aluno;
+
+UPDATE aluno
+SET nome_alu = 'Roberto Carlos'
+WHERE id_alu = 11;
+
+DELETE FROM aluno
+WHERE id_alu = 11;
