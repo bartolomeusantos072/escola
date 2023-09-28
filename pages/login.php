f30858abd76fdbbb944f9e2b505ce2ab616923cf
bartolomeusantos072 <bartolomeusantos072@gmail.com>
2023-09-27 23:56:17 -0300

    criacao dos arquivos basicos

---
+++ pages/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Login</h1>
+        <a href="site_escola.php">Acessar o Sistema</a>
+    </div>
+</body>
+</html>
---
+++ pages/site_escola.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sistema Escola</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Sistema Escola</h1>
+        <div class="menu">
+            <ul>
+                <li><a href="cadastro_aluno.php">Cadastro de Aluno</a></li>
+                <li><a href="visualizar_alunos.php">Visualização dos Alunos</a></li>
+                <li><a href="alterar_aluno.php">Alterar Dados do Aluno</a></li>
+                <li><a href="excluir_alunos.php">Excluir Alunos</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
